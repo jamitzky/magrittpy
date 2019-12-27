2bb91dc24e54f8e2a916f78008ddacb5a0b06879
Ferdinand Jamitzky <f.jamitzky@gmail.com>
2019-12-27 13:11:47 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,8 +1,14 @@
 class forall:
     def __init__(self,*args):
         self.args=args
-    def __rshift__(self,other)
-        if other.__class__ == :
-            return forall(other(*args))
-        elif other.__class == result
-            return self.args
+    def __rshift__(self,other):
+        if other == result:
+            return self.args[0]
+        elif callable(other):
+            return forall(other(*self.args))
+        
+class result:
+    pass
+
+#tests:
+assert( 285 == forall(range(10)) >> (lambda x:[i**2 for i in x]) >>sum >> result)
