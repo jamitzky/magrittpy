abe7b59687643d557042a6bb391c757847aa1f3e
Ferdinand Jamitzky <f.jamitzky@gmail.com>
2019-12-27 11:47:09 +0100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+class forall:
+    def __init__(self,*args):
+        self.args=args
+    def __rshift__(self,other)
+        if other.__class__ == :
+            return forall(other(*args))
+        elif other.__class == result
+            return self.args
